db76ebb490b3566581a9b38c54348984b17f756e
Vladislav Sokolov <79214579055@yandex.ru>
2023-10-25 22:01:04 +0300

    finished designig of the main page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,8 +11,8 @@ const Layout: React.FC<layoutProps> = ({setCity, chosenCity}) => {
     return (
         <div className="flex min-h-screen flex-row bg-gray-100 text-gray-800 font-mono text-base font-bold tracking-widest">
 			<Sidebar setCity={setCity} chosenCity={chosenCity} />
-			<main className="relative main w-[calc(100%-48rem)] -ml-48 flex flex-grow flex-col p-4 transition-all duration-150 ease-in md:ml-0">
-                <div className="relative w-[100%] px-4 py-4 flex flex-col h-full items-start justify-start bg-white text-center text-lg font-bold shadow-md">
+			<main className="relative main w-[calc(100%-48rem)] -ml-48 flex grow shrink-0 flex-col p-4 transition-all duration-150 ease-in md:ml-0">
+                <div className="relative overflow-hidden w-[100%] px-4 py-4 flex flex-col grow shrink-0 h-full items-start justify-between bg-white text-center text-lg font-bold shadow-md">
 				    <Outlet/>
                 </div>
 			</main>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -26,22 +26,24 @@ const MainPage: React.FC<mainPageProps> = ({data, isError, isLoading, city}) => 
             {!isLoading && city.length === 0 && <div className='flex justify-center items-center h-[100%] w-[100%] text-center'>You`ve not chosen any city</div>}
             {city.length > 0 && data !== undefined && 
                 <>
-                    <div className='text-center text-xl mx-auto'>{data.location.country.length > 0 && data.location.country + ', '}{data.location.region.length > 0 && data.location.region + ', '}{data.location.name.length > 0 && data.location.name}</div>
-                    <div className='text-sm text-gray-500 text-center mx-auto'>Today is {data.forecast.forecastday[0].date.replaceAll('-', '.')}</div>
-                    <ul className='myscrollbar max-w-[100%] relative flex overflow-x-auto mx-auto'>
-                        {forecastDay !== undefined && data.forecast.forecastday.map(day => (
-                            <li
-                                onClick={() => {
-                                    setForecastDay(day)
-                                }}
-                                className={forecastDay.date === day.date 
-                                    ? 'active text-sm shrink-0 my-2 mx-1 border-2 border-indigo-200 rounded-[20px] px-2 py-1 hover:bg-indigo-100 shadow-md transition-all cursor-pointer'
-                                    : 'text-sm bg-gray-900/[.02] shrink-0 my-2 mx-1 border-2 border-indigo-200 rounded-[20px] px-2 py-1 hover:bg-indigo-100 shadow-md transition-all cursor-pointer' 
-                                }
-                                key={day.date}
-                            >{day.date.replaceAll('-', '.')}</li>
-                        ))}
-                    </ul>
+                    <div className='flex flex-col items-center mx-auto'>
+                        <div className='text-center text-xl mx-auto'>{data.location.country.length > 0 && data.location.country + ', '}{data.location.region.length > 0 && data.location.region + ', '}{data.location.name.length > 0 && data.location.name}</div>
+                        <div className='text-sm text-gray-500 text-center mx-auto'>Today is {data.forecast.forecastday[0].date.replaceAll('-', '.')}</div>
+                        <ul className='myscrollbar max-w-[100%] relative flex overflow-x-auto mx-auto'>
+                            {forecastDay !== undefined && data.forecast.forecastday.map(day => (
+                                <li
+                                    onClick={() => {
+                                        setForecastDay(day)
+                                    }}
+                                    className={forecastDay.date === day.date 
+                                        ? 'active text-sm shrink-0 my-2 mx-1 border-2 border-indigo-200 rounded-[20px] px-2 py-1 hover:bg-indigo-100 shadow-md transition-all cursor-pointer'
+                                        : 'text-sm bg-gray-900/[.02] shrink-0 my-2 mx-1 border-2 border-indigo-200 rounded-[20px] px-2 py-1 hover:bg-indigo-100 shadow-md transition-all cursor-pointer' 
+                                    }
+                                    key={day.date}
+                                >{day.date.replaceAll('-', '.')}</li>
+                            ))}
+                        </ul>
+                    </div>
                     {forecastDay !== undefined && 
                         <div className='flex w-[100%] flex-col justify-start items-start'>
                             <div className='flex flex-col mx-auto items-center justify-center mb-3'>
@@ -68,7 +70,7 @@ const MainPage: React.FC<mainPageProps> = ({data, isError, isLoading, city}) => 
                             </div>
                             
                             <div className='relative mx-auto mt-5'>
-                                <div className='flex justify-center items-center w-[300px] h-[150px] border-[3px] border-indigo-500/[.7] rounded-[10em/5em] border-b-0 border-r-0 border-l-0 mx-auto'>
+                                <div className='flex justify-center items-center w-[300px] h-[120px] border-[3px] border-indigo-500/[.7] rounded-[10em/4em] border-b-0 border-r-0 border-l-0 mx-auto'>
                                     <span className='relative bg-gray-400/[.6] w-full h-[2px] block bottom-[5px]'></span>
                                 </div>
                                 <div className='absolute w-[50px] left-0 top-[40%] translate-x-[-75%] flex flex-col justify-center items-center'>
@@ -82,7 +84,7 @@ const MainPage: React.FC<mainPageProps> = ({data, isError, isLoading, city}) => 
                             </div>
                         </div>
                     }
-                    <ul className='myscrollbar max-w-[100%] relative flex overflow-x-auto'>
+                    <ul className='myscrollbar max-w-[100%] flex overflow-x-auto'>
                         {forecastDay !== undefined && forecastDay.hour.map(hour => (
                                 <li
                                     className='bg-gray-900/[.02] text-sm shrink-0 my-2 mx-1 border-2 border-indigo-200 rounded-[20px] px-2 py-1 shadow-md transition-all' 
